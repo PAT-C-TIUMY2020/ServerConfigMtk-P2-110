1d7113ca6cf97f356d15aeb77ff747975399ea76
Nur Afandi Mujib <fndmujib@gmail.com>
2020-11-07 14:37:37 +0700

    Pertemuan 6

---
+++ ServerConfigMtk-P2-110/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_110;
+
+namespace ServerConfigMtk_P2_110
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server Nyalaaaaa!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
